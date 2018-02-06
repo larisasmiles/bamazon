a5c7bcdbd9c872fd8e6b0275374f1fcb4ad1269e
Larisasmiles <larisasmiles@gmail.com>
2018-02-06 14:44:57 -0700

    js code

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,6 +11,20 @@ const connection = mysql.createConnection({
 connection.connect(function(err) {
   console.log("connected as id: "+connection.threadId);
 })
+
+const start = function() {
+  inquirer.prompt([{
+    name: "productSelection",
+    type: "input",
+    message: "What is the [ID] number of the [product] you'd like to buy?",
+  },
+  {
+    name: "quanity",
+    type: "input",
+    message: "How many would you like?"
+  }
+  
+
 // Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
 
 // 6. The app should then prompt users with two messages.
