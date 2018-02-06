1958d3e7725a164f2a77e8abefb4793af2bad629
Larisasmiles <larisasmiles@gmail.com>
2018-02-06 14:28:07 -0700

    stylesheet and html

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,3 +11,9 @@ const connection = mysql.createConnection({
 connection.connect(function(err) {
   console.log("connected as id: "+connection.threadId);
 })
+// Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
+
+// 6. The app should then prompt users with two messages.
+
+// * The first should ask them the ID of the product they would like to buy.
+// * The second message should ask how many units of the product they would like to buy.
