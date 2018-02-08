1127fcd2c125696fc4216d3d4ca2b719d5ddc179
Larisasmiles <larisasmiles@gmail.com>
2018-02-08 09:47:33 -0700

    error coming when using prompt

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,6 +12,24 @@ connection.connect(function(err) {
   console.log("connected as id: "+connection.threadId);
 })
 
+
+// // global variables
+// let currectStock = 0;
+// let price = 0;
+
+// function displayItems() {
+//   console.log("Items In Stock")
+
+//   connection.query(
+//     'SELECT * FROM `products` WHERE stock_quanitity > 0',
+//     function (err, results, fields) {
+//       var table = new Table({
+
+//       })
+//     }
+//   )
+// }
+ 
 const start = function() {
   inquirer.prompt([{
     name: "productSelection",
@@ -22,12 +40,41 @@ const start = function() {
     name: "quanity",
     type: "input",
     message: "How many would you like?"
-  }
+  }].then(function(answer){
+    if(answer.productSelection()=="")
+  })
+)
   
+};
 
-// Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
+console.log(start);
 
-// 6. The app should then prompt users with two messages.
 
-// * The first should ask them the ID of the product they would like to buy.
-// * The second message should ask how many units of the product they would like to buy.
+// need inquirer done
+// need mysql done
+
+// connect to mysql database done
+
+// create menu, via inquirer
+// products for sale (functions)
+// low inventory (functions)
+// add to inventory (functions)
+// add new product (functions)
+// switch statement, call the above functions
+
+
+// function currentProducts()
+// query database, select every item inside db
+// list ids, names, prices, and quantities
+
+// function lowInventory()
+// list all items with inventory count < 5
+
+// function addInventory()
+// prompt (inquirer) manager to add more
+// items to current product
+// select current item, update sql db
+
+// function newProduct()
+// add new product to db
+// inquirer to prompt item name, cost, department, etc match to your db
