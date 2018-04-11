b6042ef76943ca1e5b282431f8a364b00b3ea3c4
Larisasmiles <larisasmiles@gmail.com>
2018-04-11 17:30:48 -0600

    bamazon customer is working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,80 +1,136 @@
-const mysql = require('mysql');
-const inquirer = require('inquirer');
+// Initializes the npm packages used
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+require("console.table");
 
-const connection = mysql.createConnection({
-    host:"localhost",
-    port: 3306,
-    user: "root",
-    database:"bamazon"
-})
+// Initializes the connection variable to sync with a MySQL database
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
 
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "root",
+  database: "bamazon"
+});
+
+// Creates the connection with the server and loads the product data upon a successful connection
 connection.connect(function(err) {
-  console.log("connected as id: "+connection.threadId);
-})
+  if (err) {
+    console.error("error connecting: " + err.stack);
+  }
+  loadProducts();
+});
 
+// Function to load the products table from the database and print results to the console
+function loadProducts() {
+  // Selects all of the data from the MySQL products table
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
 
-// // global variables
-// let currectStock = 0;
-// let price = 0;
+    // Draw the table in the terminal using the response
+    console.table(res);
 
-// function displayItems() {
-//   console.log("Items In Stock")
+    // Then prompt the customer for their choice of product, pass all the products to promptCustomerForItem
+    promptCustomerForItem(res);
+  });
+}
 
-//   connection.query(
-//     'SELECT * FROM `products` WHERE stock_quanitity > 0',
-//     function (err, results, fields) {
-//       var table = new Table({
+// Prompt the customer for a product ID
+function promptCustomerForItem(inventory) {
+  // Prompts user for what they would like to purchase
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "choice",
+        message: "What is the ID of the item you would you like to purchase? [Quit with Q]",
+        validate: function(val) {
+          return !isNaN(val) || val.toLowerCase() === "q";
+        }
+      }
+    ])
+    .then(function(val) {
+      // Check if the user wants to quit the program
+      checkIfShouldExit(val.choice);
+      var choiceId = parseInt(val.choice);
+      var product = checkInventory(choiceId, inventory);
 
-//       })
-//     }
-//   )
-// }
- 
-const start = function() {
-  inquirer.prompt([{
-    name: "productSelection",
-    type: "input",
-    message: "What is the [ID] number of the [product] you'd like to buy?",
-  },
-  {
-    name: "quanity",
-    type: "input",
-    message: "How many would you like?"
-  }].then(function(answer){
-    if(answer.productSelection()=="")
-  })
-)
-  
-};
+      // If there is a product with the id the user chose, prompt the customer for a desired quantity
+      if (product) {
+        // Pass the chosen product to promptCustomerForQuantity
+        promptCustomerForQuantity(product);
+      }
+      else {
+        // Otherwise let them know the item is not in the inventory, re-run loadProducts
+        console.log("\nThat item is not in the inventory.");
+        loadProducts();
+      }
+    });
+}
 
-console.log(start);
+// Prompt the customer for a product quantity
+function promptCustomerForQuantity(product) {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "quantity",
+        message: "How many would you like? [Quit with Q]",
+        validate: function(val) {
+          return val > 0 || val.toLowerCase() === "q";
+        }
+      }
+    ])
+    .then(function(val) {
+      // Check if the user wants to quit the program
+      checkIfShouldExit(val.quantity);
+      var quantity = parseInt(val.quantity);
 
+      // If there isn't enough of the chosen product and quantity, let the user know and re-run loadProducts
+      if (quantity > product.stock_quantity) {
+        console.log("\nInsufficient quantity!");
+        loadProducts();
+      }
+      else {
+        // Otherwise run makePurchase, give it the product information and desired quantity to purchase
+        makePurchase(product, quantity);
+      }
+    });
+}
 
-// need inquirer done
-// need mysql done
+// Purchase the desired quantity of the desired item
+function makePurchase(product, quantity) {
+  connection.query(
+    "UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?",
+    [quantity, product.item_id],
+    function(err, res) {
+      // Let the user know the purchase was successful, re-run loadProducts
+      console.log("\nSuccessfully purchased " + quantity + " " + product.product_name + "'s!");
+      loadProducts();
+    }
+  );
+}
 
-// connect to mysql database done
+// Check to see if the product the user chose exists in the inventory
+function checkInventory(choiceId, inventory) {
+  for (var i = 0; i < inventory.length; i++) {
+    if (inventory[i].item_id === choiceId) {
+      // If a matching product is found, return the product
+      return inventory[i];
+    }
+  }
+  // Otherwise return null
+  return null;
+}
 
-// create menu, via inquirer
-// products for sale (functions)
-// low inventory (functions)
-// add to inventory (functions)
-// add new product (functions)
-// switch statement, call the above functions
-
-
-// function currentProducts()
-// query database, select every item inside db
-// list ids, names, prices, and quantities
-
-// function lowInventory()
-// list all items with inventory count < 5
-
-// function addInventory()
-// prompt (inquirer) manager to add more
-// items to current product
-// select current item, update sql db
-
-// function newProduct()
-// add new product to db
-// inquirer to prompt item name, cost, department, etc match to your db
+// Check to see if the user wants to quit the program
+function checkIfShouldExit(choice) {
+  if (choice.toLowerCase() === "q") {
+    // Log a message and exit the current node process
+    console.log("Goodbye!");
+    process.exit(0);
+  }
+}
